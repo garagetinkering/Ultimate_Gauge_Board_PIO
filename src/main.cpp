e1aa4396671cb156720ab28a39fd16ca8d6368e4
valentineautos <valentine.autos@gmail.com>
2025-09-12 09:57:01 +0100

    Bowing to pressure and refactored all functions to snake_case

--- src/main.cpp
+++ src/main.cpp
@@ -48,13 +48,13 @@ lv_obj_t *needle_img;
 
 lv_obj_t *scale_ticks[SCALE_TICKS_COUNT];
 
-void Drivers_Init(void) {
-  I2C_Init();
+void drivers_init(void) {
+  i2c_init();
 
   Serial.println("Scanning for TCA9554...");
   bool found = false;
   for (int attempt = 0; attempt < 10; attempt++) {
-    if (I2C_ScanAddress(0x20)) { // 0x20 is default for TCA9554
+    if (i2c_scan_address(0x20)) { // 0x20 is default for TCA9554
       found = true;
       break;
     }
@@ -64,80 +64,80 @@ void Drivers_Init(void) {
   if (!found) {
     Serial.println("TCA9554 not detected! Skipping expander init.");
   } else {
-    TCA9554PWR_Init(0x00);
+    tca9554pwr_init(0x00);
   }
-  LCD_Init();
-  CANBus_Init();
-  Lvgl_Init();
+  lcd_init();
+  canbus_init();
+  lvgl_init();
 }
 
 // HELPER FUNCTIONS
 
 // create moving average for smoothing
-uint8_t Get_Moving_Average(int new_value) {
-    static int values[AVERAGE_VALUES] = {0};
-    static int index = 0;
-    static int count = 0;
-    static double sum = 0;
+uint8_t get_moving_average(int new_value) {
+  static int values[AVERAGE_VALUES] = {0};
+  static int index = 0;
+  static int count = 0;
+  static double sum = 0;
 
-    // Subtract the value being replaced
-    sum -= values[index];
+  // Subtract the value being replaced
+  sum -= values[index];
 
-    // Insert the new value
-    values[index] = new_value;
-    sum += new_value;
+  // Insert the new value
+  values[index] = new_value;
+  sum += new_value;
 
-    // Update index and count
-    index = (index + 1) % AVERAGE_VALUES;
-    if (count < AVERAGE_VALUES) count++;
+  // Update index and count
+  index = (index + 1) % AVERAGE_VALUES;
+  if (count < AVERAGE_VALUES) count++;
 
-    return std::round(sum / count);
+  return std::round(sum / count);
 }
 
-void Update_Coolant_Temp(void) {
+void update_coolant_temp(void) {
   // use a moving average of the last x values for smoothing
-  average_coolant_temp = Get_Moving_Average(GaugeData.coolant_temp);
+  average_coolant_temp = get_moving_average(GaugeData.coolant_temp);
   if (init_anim_complete) {
     lv_scale_set_image_needle_value(scale, needle_img, average_coolant_temp);
   }
 }
 
 // update parts with incoming values
-void Update_Values(void) {
-  Update_Coolant_Temp();
+void update_values(void) {
+  update_coolant_temp();
 }
 
 // update the UI with the latest value
-static void Set_Needle_Img_Value(void * obj, int32_t v) {
+static void set_needle_img_value(void * obj, int32_t v) {
   lv_scale_set_image_needle_value(scale, needle_img, v);
 }
 
 // mark the loader as complete
-void Needle_Sweep_Complete(lv_anim_t *a) {
-    init_anim_complete = true;
+void needle_sweep_complete(lv_anim_t *a) {
+  init_anim_complete = true;
 }
 
 // scroll the needle from 0 to the current value
-void Needle_To_Current(lv_anim_t *a) {
+void needle_to_current(lv_anim_t *a) {
   lv_anim_t anim_scale_img;
   lv_anim_init(&anim_scale_img);
   lv_anim_set_var(&anim_scale_img, scale);
-  lv_anim_set_exec_cb(&anim_scale_img, Set_Needle_Img_Value);
+  lv_anim_set_exec_cb(&anim_scale_img, set_needle_img_value);
   lv_anim_set_duration(&anim_scale_img, 1000);  
   lv_anim_path_ease_out(&anim_scale_img);
   lv_anim_set_values(&anim_scale_img, -20, average_coolant_temp);
-  lv_anim_set_ready_cb(&anim_scale_img, Needle_Sweep_Complete);
+  lv_anim_set_ready_cb(&anim_scale_img, needle_sweep_complete);
   lv_anim_start(&anim_scale_img);
 }
 
 // 0 to max to 0 sweep on load
-void Needle_Sweep() {
+void needle_sweep() {
   if (TESTING) {
     // back and forth sweep for testing
     lv_anim_t anim_scale_img;
     lv_anim_init(&anim_scale_img);
     lv_anim_set_var(&anim_scale_img, scale);
-    lv_anim_set_exec_cb(&anim_scale_img, Set_Needle_Img_Value);
+    lv_anim_set_exec_cb(&anim_scale_img, set_needle_img_value);
     lv_anim_set_duration(&anim_scale_img, 10000);
     lv_anim_set_repeat_count(&anim_scale_img, LV_ANIM_REPEAT_INFINITE);
     lv_anim_set_playback_duration(&anim_scale_img, 10000);
@@ -147,17 +147,17 @@ void Needle_Sweep() {
     lv_anim_t anim_scale_img;
     lv_anim_init(&anim_scale_img);
     lv_anim_set_var(&anim_scale_img, scale);
-    lv_anim_set_exec_cb(&anim_scale_img, Set_Needle_Img_Value);
+    lv_anim_set_exec_cb(&anim_scale_img, set_needle_img_value);
     lv_anim_set_duration(&anim_scale_img, 2000);
     lv_anim_set_repeat_count(&anim_scale_img, 1);
     lv_anim_set_playback_duration(&anim_scale_img, 1000);
     lv_anim_set_values(&anim_scale_img, SCALE_MIN, SCALE_MAX);
-    lv_anim_set_ready_cb(&anim_scale_img, Needle_To_Current);
+    lv_anim_set_ready_cb(&anim_scale_img, needle_to_current);
     lv_anim_start(&anim_scale_img);
   }    
 }
 
-void Make_Scale_Ticks(void) {
+void make_scale_ticks(void) {
   for (int i = 0; i < SCALE_TICKS_COUNT; i++) {
         scale_ticks[i] = lv_image_create(main_scr);
         lv_image_set_src(scale_ticks[i], &tabby_tick);
@@ -174,68 +174,65 @@ void Make_Scale_Ticks(void) {
 }
 
 // create the elements on the main scr
-void Main_Scr_UI(void) {
-    // scale used for needle
-    scale = lv_scale_create(main_scr);
-    lv_obj_set_size(scale, 480, 480);
-    lv_scale_set_mode(scale, LV_SCALE_MODE_ROUND_INNER);
-    lv_obj_set_style_bg_opa(scale, LV_OPA_0, 0);
-    lv_scale_set_total_tick_count(scale, 0);
-    lv_scale_set_label_show(scale, false);
-    lv_obj_center(scale);
-    lv_scale_set_range(scale, SCALE_MIN, SCALE_MAX);
-    lv_scale_set_angle_range(scale, 240);
-    lv_scale_set_rotation(scale, 90);
+void main_scr_ui(void) {
+  // scale used for needle
+  scale = lv_scale_create(main_scr);
+  lv_obj_set_size(scale, 480, 480);
+  lv_scale_set_mode(scale, LV_SCALE_MODE_ROUND_INNER);
+  lv_obj_set_style_bg_opa(scale, LV_OPA_0, 0);
+  lv_scale_set_total_tick_count(scale, 0);
+  lv_scale_set_label_show(scale, false);
+  lv_obj_center(scale);
+  lv_scale_set_range(scale, SCALE_MIN, SCALE_MAX);
+  lv_scale_set_angle_range(scale, 240);
+  lv_scale_set_rotation(scale, 90);
 
-    lv_obj_t *lower_arc = lv_arc_create(main_scr);
-    lv_obj_set_size(lower_arc, 412, 412);
-    lv_arc_set_bg_angles(lower_arc, 90, 90 + (240 / (SCALE_TICKS_COUNT - 1)));
-    lv_arc_set_value(lower_arc, 0);
-    lv_obj_center(lower_arc);
-    lv_obj_set_style_opa(lower_arc, 0, LV_PART_KNOB);
-    lv_obj_set_style_arc_color(lower_arc, lv_color_make(87,10,1), LV_PART_MAIN);
-    lv_obj_set_style_arc_width(lower_arc, 16, LV_PART_MAIN);
-    lv_obj_set_style_arc_rounded(lower_arc, false, LV_PART_MAIN);
+  lv_obj_t *lower_arc = lv_arc_create(main_scr);
+  lv_obj_set_size(lower_arc, 412, 412);
+  lv_arc_set_bg_angles(lower_arc, 90, 90 + (240 / (SCALE_TICKS_COUNT - 1)));
+  lv_arc_set_value(lower_arc, 0);
+  lv_obj_center(lower_arc);
+  lv_obj_set_style_opa(lower_arc, 0, LV_PART_KNOB);
+  lv_obj_set_style_arc_color(lower_arc, lv_color_make(87,10,1), LV_PART_MAIN);
+  lv_obj_set_style_arc_width(lower_arc, 16, LV_PART_MAIN);
+  lv_obj_set_style_arc_rounded(lower_arc, false, LV_PART_MAIN);
 
-    lv_obj_t *upper_arc = lv_arc_create(main_scr);
-    lv_obj_set_size(upper_arc, 412, 412);
-    lv_arc_set_bg_angles(upper_arc, 330 - (240 / (SCALE_TICKS_COUNT - 1)), 330);
-    lv_arc_set_value(upper_arc, 0);
-    lv_obj_center(upper_arc);
-    lv_obj_set_style_opa(upper_arc, 0, LV_PART_KNOB);
-    lv_obj_set_style_arc_color(upper_arc, lv_color_make(87,10,1), LV_PART_MAIN);
-    lv_obj_set_style_arc_width(upper_arc, 16, LV_PART_MAIN);
-    lv_obj_set_style_arc_rounded(upper_arc, false, LV_PART_MAIN);
+  lv_obj_t *upper_arc = lv_arc_create(main_scr);
+  lv_obj_set_size(upper_arc, 412, 412);
+  lv_arc_set_bg_angles(upper_arc, 330 - (240 / (SCALE_TICKS_COUNT - 1)), 330);
+  lv_arc_set_value(upper_arc, 0);
+  lv_obj_center(upper_arc);
+  lv_obj_set_style_opa(upper_arc, 0, LV_PART_KNOB);
+  lv_obj_set_style_arc_color(upper_arc, lv_color_make(87,10,1), LV_PART_MAIN);
+  lv_obj_set_style_arc_width(upper_arc, 16, LV_PART_MAIN);
+  lv_obj_set_style_arc_rounded(upper_arc, false, LV_PART_MAIN);
     
     
-    Make_Scale_Ticks();
+  make_scale_ticks();
     
-    // needle image
-    int needle_center_shift = 40; // how far the center of the needle is shifted from the left edge
+  // needle image
+  int needle_center_shift = 40; // how far the center of the needle is shifted from the left edge
    
-    needle_img = lv_image_create(scale);
-    lv_image_set_src(needle_img, &tabby_needle);
-    lv_obj_align(needle_img, LV_ALIGN_CENTER, 108 - needle_center_shift, 0);
-    lv_image_set_pivot(needle_img, needle_center_shift, 36);
+  needle_img = lv_image_create(scale);
+  lv_image_set_src(needle_img, &tabby_needle);
+  lv_obj_align(needle_img, LV_ALIGN_CENTER, 108 - needle_center_shift, 0);
+  lv_image_set_pivot(needle_img, needle_center_shift, 36);
 
     lv_obj_set_style_image_recolor_opa(needle_img, 255, 0);
     lv_obj_set_style_image_recolor(needle_img, lv_color_make(255,255,255), 0); // TO DO - replace with color from CAN 
 }
 
 // build the screens
-void Screens_Init(void) {
+void screens_init(void) {
   main_scr = lv_obj_create(NULL);
   lv_obj_set_style_bg_color(main_scr, lv_color_make(0,0,0), 0);
   lv_screen_load(main_scr);
 
-  Main_Scr_UI();
+  main_scr_ui();
 }
 
 // process incoming CAN coolante temp message
-void Process_Coolant_Temp(uint8_t *byte_data) {
-    // byte 0
-    // modifier -40
-
+void process_coolant_temp(uint8_t *byte_data) {
     int byte_pos = 0;
     int final_temp = byte_data[byte_pos] - 40; // reduce value by 40 (Nissan specific)
 
@@ -245,7 +242,7 @@ void Process_Coolant_Temp(uint8_t *byte_data) {
 }
 
 // task to avoid blocking LVGL with CAN receive
-void Receive_CAN_Task(void *arg) {
+void receive_can_task(void *arg) {
   // Register this task with the watchdog
   esp_task_wdt_add(NULL);
 
@@ -269,14 +266,14 @@ void Receive_CAN_Task(void *arg) {
         // process CAN IDs
         switch (message.identifier) {
           case 0x551:
-            Process_Coolant_Temp(message.data);
+            process_coolant_temp(message.data);
             data_ready = true;
             break;
           default:
             break;
         }
 
-        Set_EXIO(EXIO_PIN4, High);
+  set_exio(EXIO_PIN4, High);
     } else if (err == ESP_ERR_TIMEOUT) {
         // No message within 100ms — that’s fine
         ESP_LOGW(TAG, "Reception timed out");
@@ -293,14 +290,12 @@ void Receive_CAN_Task(void *arg) {
 void setup(void) {
   Serial.begin(115200);
   Serial.println("begin");
-  Drivers_Init();
-  Set_Backlight(100);
-
-  Screens_Init();
-  Needle_Sweep();
-  Set_EXIO(EXIO_PIN4, Low);
-
-  xTaskCreate(Receive_CAN_Task, "Receive_CAN_Task", 8192, NULL, 1, NULL);
+  drivers_init();
+  set_backlight(100);
+  screens_init();
+  needle_sweep();
+  set_exio(EXIO_PIN4, Low);
+  xTaskCreate(receive_can_task, "Receive_CAN_Task", 8192, NULL, 1, NULL);
   Serial.print("setup complete");
 }
 
@@ -310,7 +305,7 @@ void loop(void) {
   // only update the display if new data has arrived
   if (data_ready) {
     data_ready = false;
-    Update_Values();
+  update_values();
   }
 
   delay(5);
